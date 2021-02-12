b35be489022a2e5c84c7586b7fc3172c248d47d8
Med Amine <xxxcodeshotxxx@gmail.com>
2021-02-12 10:41:06 +0100

    another commit

--- config/settings.py
+++ config/settings.py
@@ -3,12 +3,12 @@ from os import urandom,environ
 class Config:
     DEBUG = False
     PORT = environ.get('PORT') or 5005
-    
-
+    SECRET_KEY = urandom(32)
+    ENV = environ.get('ENV')
 
 class Dev(Config):
-    pass
+    DEBUG = True
 
 
 class Prod(Config):
-    pass
+    PORT = environ.get('PORT') or 8080
